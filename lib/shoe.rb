86aaf5193ef3b550ebd15d92ff02244523b8ce56
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-06 01:58:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,16 @@
 class Shoe
-  
- BRANDS = [] 
-  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
